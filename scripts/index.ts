2266e7006caba56ca53ff54194146a1ad7bd0388
parksb <parkgds@gmail.com>
2020-12-24 00:59:47 +0900

    문서 breadcrumb 추가

--- scripts/index.ts
+++ scripts/index.ts
@@ -17,6 +17,14 @@ const DIST_DIRECTORY_PATH: string = path.join(__dirname, '../build');
 const TEMPLATE_FILE_PATH: Buffer = fs.readFileSync(path.join(__dirname, './index.ejs'));
 const SITEMAP_PATH = `${DIST_DIRECTORY_PATH}/sitemap.xml`;
 
+interface Document {
+  title: string;
+  filename: string; // without extension
+  html: string;
+  breadcrumbs: string[]; // without extension
+  children: Document[];
+}
+
 (() => {
   const md: MarkdownIt = new MarkdownIt({
     html: false,
@@ -47,33 +55,55 @@ const SITEMAP_PATH = `${DIST_DIRECTORY_PATH}/sitemap.xml`;
       disabled: true,
     });
 
-    const documentFiles: string[] = fs.readdirSync(MARKDOWN_DIRECTORY_PATH)
-    .filter((file) => !file.startsWith('.') );
+    // https://github.com/johngrib/johngrib-jekyll-skeleton/blob/v1.0/_includes/createLink.html
+    const linkRegex = /\[\[(.+?)\]\]/g;
+    const labeledLinkRegex = /\[\[(.+?)\]\]\{(.+?)\}/g;
 
-    const documents = documentFiles.map((filename: string) => {
+    const findInternalLinks = (markdown: string) => {
+      return [
+        ...markdown.match(linkRegex)?.map((link) => link.replace(/(\[\[)|(\]\])/g, '')) || [],
+        ...markdown.match(labeledLinkRegex)?.map((link) => link.replace(/(\[\[)|(\]\])|({(.+?)})/g, '')) || [],
+      ];
+    };
+    
+    const parseMarkdown = (filename: string, breadcrumbs: string[]): Document => {
       const preContents = '[[toc]]\n\n';
-      const markdown = `${preContents}${fs.readFileSync(`${MARKDOWN_DIRECTORY_PATH}/${filename}`)}`;
-      const html = md.render(markdown) // https://github.com/johngrib/johngrib-jekyll-skeleton/blob/v1.0/_includes/createLink.html
-        .replace(/\[\[(.+?)\]\]\{(.+?)\}/g, '<a href="$1.html">$2</a>')
-        .replace(/\[\[(.+?)\]\]/g, '<a href="$1.html">$1</a>');
-      const filenameWithoutExt = filename.replace('.md', '');
-      const title = markdown.match(/#\s.*/)[0].replace(/^#\s/, '');
-      return { title, filename, filenameWithoutExt, html };
-    });
+      const markdown = fs.readFileSync(`${MARKDOWN_DIRECTORY_PATH}/${filename}.md`).toString();
 
-    documents.forEach((document) => {
+      const html = md.render(`${preContents}${markdown}`)
+        .replace(labeledLinkRegex, '<a href="$1.html">$2</a>')
+        .replace(linkRegex, '<a href="$1.html">$1</a>');
+
+      const links = findInternalLinks(markdown);
+      const children = links.map((link) => parseMarkdown(link, [...breadcrumbs, link]));
+
+      const title = markdown.match(/^#\s.*/)[0].replace(/^#\s/, '');
+      return { title, filename, html, breadcrumbs, children };
+    };
+
+    const writeHTML = (document: Document) => {
       fs.writeFileSync(
-        `${DIST_DIRECTORY_PATH}/${document.filenameWithoutExt}.html`,
+        `${DIST_DIRECTORY_PATH}/${document.filename}.html`,
         ejs.render(String(TEMPLATE_FILE_PATH), { document }),
       );
-    });
 
-    const documentUrls = documents.map((document) => `<url><loc>https://wikiwikiwi.vercel.app/${document.filenameWithoutExt}.html</loc><changefreq>daily</changefreq><priority>1.00</priority></url>`);
-    const content = `<?xml version="1.0" encoding="UTF-8"?>
+      document.children.map((child) => writeHTML(child));
+    };
+
+    const writeSitemap = () => {
+      const htmlFiles = fs.readdirSync(DIST_DIRECTORY_PATH).filter((file) => !file.startsWith('.'));
+      const urls = htmlFiles.map((file) => `<url><loc>https://wikiwikiwi.vercel.app/${file}</loc><changefreq>daily</changefreq><priority>1.00</priority></url>`);
+      const content = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 <url><loc>https://wikiwikiwi.vercel.app/</loc><changefreq>daily</changefreq><priority>1.00</priority></url>
-${documentUrls.join('\n')}
+${urls.join('\n')}
 </urlset>
 `;
-    fs.writeFileSync(SITEMAP_PATH, content);
+      fs.writeFileSync(SITEMAP_PATH, content);
+    };
+
+    const root = parseMarkdown('index', []);
+
+    writeHTML(root);
+    writeSitemap();
 })();
